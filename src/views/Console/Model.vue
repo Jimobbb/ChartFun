499fb359d01fa0a1dfaa6884702dfdc11e162dba
Jimobbb <1768991760@qq.com>
2023-04-28 10:16:18 +0800

    新增模型界面

---
+++ src/views/Console/Model.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  div
+    el-row(style="margin-bottom: 20px;")
+      el-button(type="primary" @click="addModel") 新增模型
+</template>
+
+<script>
+// export default {
+//   props: ['user'],
+//   data() {
+//     return {
+//       connectList: [],
+//     },
+//   }
+// };
+</script>
+
+<style lang="scss" scoped>
+</style>
